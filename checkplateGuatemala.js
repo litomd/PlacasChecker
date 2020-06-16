f23cfa29eee4b886093bf84a6b50e5e4c2ff5c63
Leonel Vinicio Morales Díaz <litomd@gmail.com>
2020-06-16 06:49:50 -0600

    Create checkplateGuatemala.js
    Simple plate number checker. Checks if plate number allows vehicle to circulate in Guatemala according to government restrictions for June 2020.

---
+++ checkplateGuatemala.js
@@ -0,0 +1,5 @@
+/***********************************************************/
+/* Based on algorithm in:
+/* https://twitter.com/litomd/status/1272476258358747136
+/***********************************************************/
+console.log((parseInt("341GMQ")+(new Date()).getDate())%2===0?"Sí circula":"No circula");
